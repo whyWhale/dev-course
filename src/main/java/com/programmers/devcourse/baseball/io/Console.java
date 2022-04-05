8690f7e510753fdc9b6598e22f5a82182ad0368e
yeon <soccer_by@naver.com>
2022-04-05 12:44:46 +0900

    refactor: convention 적용

--- src/main/java/com/programmers/devcourse/baseball/io/Console.java
+++ src/main/java/com/programmers/devcourse/baseball/io/Console.java
@@ -8,22 +8,21 @@ import java.io.OutputStreamWriter;
 
 public class Console implements Input, Output {
 
-    private static final BufferedReader reader = new BufferedReader(
-        new InputStreamReader(System.in));
-    private static final BufferedWriter writer = new BufferedWriter(
-        new OutputStreamWriter(System.out));
+	private static final BufferedReader reader = new BufferedReader(
+			new InputStreamReader(System.in));
+	private static final BufferedWriter writer = new BufferedWriter(
+			new OutputStreamWriter(System.out));
 
-    @Override
-    public String read() throws IOException {
-        return reader.readLine();
-    }
+	@Override
+	public String read() throws IOException {
+		return reader.readLine();
+	}
 
-    @Override
-    public void print(String message) throws IOException {
-        writer.write(message);
-        writer.flush();
-        writer.newLine();
-    }
-
+	@Override
+	public void print(String message) throws IOException {
+		writer.write(message + "\n");
+		writer.flush();
+		writer.newLine();
+	}
 
 }
